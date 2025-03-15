715aa5d8b2a813a855b8a34c0851037c849c7508
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-03-15 19:32:14 -0400

    Reordering projects, Variables content created
    Reordering Variables and System.Console projects. Adding contents and comments to Variables project.

---
+++ 1 - CSharpFundamentals/01 - Variables/Program.cs
@@ -0,0 +1,26 @@
+﻿// Used for counting things - e.g. Number of customers in a store
+int totalCustomers = 150;
+
+// Used for very large whole numbers - e.g. Total YouTube views
+long totalYouTubeViews = 10000000000000;
+
+// Used for small floating-point values when precision is not critical - e.g. Weight of a product
+float productWeight = 2.45f;
+
+// Used for large floating-point values where precision is not critical - e.g. Distance between planets
+double planetDistance = 384447588448949000.567;
+
+// Used for financial calculations requiring high precision - e.g. Bank account balance
+decimal accountBalance = 1250.75m;
+
+// Used for conditions - e.g. User login status
+bool isUserLoggedIn = true;
+
+// Used for single characters - e.g. Student grade
+char grade = 'A';
+
+// Used when the type needs to be inferred
+var isWeekend = false;
+
+// Used when the type needs to be inferred
+var temperature = 25;
