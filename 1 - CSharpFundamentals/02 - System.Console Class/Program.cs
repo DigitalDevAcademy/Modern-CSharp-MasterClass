f140a35a8a9c39e8a0b19f1df2363aea7dc003bc
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-03-15 21:03:41 -0400

    Renaming, System.Console Class content created
    Renaming projects. Adding content to System.Console Class project.

---
+++ 1 - CSharpFundamentals/02 - System.Console Class/Program.cs
@@ -0,0 +1,10 @@
+﻿var name = Console.ReadLine();
+var age = Console.ReadLine();
+
+Console.WriteLine("User Data");
+Console.Write(name);
+Console.Write(age);
+
+Console.Clear();
+
+Console.WriteLine("User Data Removed");
