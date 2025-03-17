3b5a2272a24d55e472d1a63162a57a45bf2074d7
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-03-17 13:23:46 -0400

    Renaming, Expressions content modified
    Modifying content and comments for Expressions project.

---
+++ 1 - CSharpFundamentals/03 - Expressions/Program.cs
@@ -0,0 +1,20 @@
+﻿// These are only some of the examples
+// Full list of operators can be found at https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/
+
+// Basic expression with additive binary operator
+int sum = 5 + 5;
+Console.WriteLine(sum);
+
+// Basic expression with additive binary operator and variables
+int number1 = 5;
+int number2 = 5;
+int sum2 = number1 + number2;
+Console.WriteLine(sum);
+
+// Basic expression with logical not unary opeartor
+bool isValid = !false;
+Console.WriteLine(isValid);
+
+// Mixed expression with additive and multiplicative operators
+var result = 5 + 5 * 10;
+Console.WriteLine(result);
