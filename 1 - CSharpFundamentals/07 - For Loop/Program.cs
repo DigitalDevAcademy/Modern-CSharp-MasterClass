f7388bcea844cdff4fb081be6fce14ee575aa624
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-03-24 17:52:05 -0400

    For Loop content created
    Adding content to For Loop project.

--- 1 - CSharpFundamentals/07 - For Loop/Program.cs
+++ 1 - CSharpFundamentals/07 - For Loop/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Forward Iteration ---");
+for (int page = 1; page <= 5; page++)
+{
+    Console.WriteLine($"Page {page}");
+}
+
+Console.WriteLine("--- Backward Iteration ---");
+for (int page = 5; page >= 1; page--)
+{
+    Console.WriteLine($"Page {page}");
+}
