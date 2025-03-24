9a1d949b2adbbba4f87532dbbf5a4ab68a947897
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-03-24 19:31:12 -0400

    Break And Continue content created
    Adding content and comments to Break And Continue project.

--- 1 - CSharpFundamentals/10 - Break And Continue/Program.cs
+++ 1 - CSharpFundamentals/10 - Break And Continue/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("--- Loop With Break ---");
+for (int page = 1; page <= 5; page++)
+{
+    if (page == 3) // Stop the loop when page is 3
+    {
+        break;
+    }
+
+    Console.WriteLine($"Page {page}");
+}
+
+Console.WriteLine("--- Loop With Continue ---");
+for (int page = 1; page <= 5; page++)
+{
+    if (page == 3) // Skip page 3 and continue the loop
+    {
+        continue;
+    }
+
+    Console.WriteLine($"Page {page}");
+}
