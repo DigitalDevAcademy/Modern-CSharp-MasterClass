5c3e7501c510b21f0ca0aa105ee03a940a1b0cea
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-01 21:01:29 -0400

    Console messages removed
    Because the Break and Continue projects are now separate there is no reason to display a message for clarity.

--- 1 - CSharpFundamentals/11 - Continue/Program.cs
+++ 1 - CSharpFundamentals/11 - Continue/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Only showcasing continue in for loop, however it can be used in other types of loops
 
-Console.WriteLine("--- Loop With Continue ---");
 for (int page = 1; page <= 5; page++)
 {
     if (page == 3) // Skip page 3 and continue the loop
