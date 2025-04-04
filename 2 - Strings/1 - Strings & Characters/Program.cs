15b4d9f7bb785d2fc8e01afd078ff9e19b54116a
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-03 22:20:55 -0400

    Strings & Characters content updated
    Adding content to Strings & Characters project.

--- 2 - Strings/1 - Strings & Characters/Program.cs
+++ 2 - Strings/1 - Strings & Characters/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Character - single symbol, uses single quotes
+char letter = 'A';
+Console.WriteLine(letter);
+
+// String - sequence of characters, uses double quotes
+string word = "Apple";
+Console.WriteLine(word);
+
+// Accessing a character from the string using indexing
+char firstLetter = word[0];
+Console.WriteLine(firstLetter);
+
+// Getting the ASCII code of a character
+int asciiCode = (int)firstLetter;
+Console.WriteLine(asciiCode);
+
+// Converting a char to string
+string firstLetterAsString = firstLetter.ToString();
+Console.WriteLine(firstLetterAsString);
+
+// Please note that char and string are different types
+// char wrong = "A";            // This would cause an error
+// string alsoWrong = 'C';      // This would also cause an error
