2bf86f9d1a1ef67fef983e84ea98249206ace1d9
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-07 17:40:39 -0400

    Strings content created
    Most required content added for Strings project. Others added as placeholders to be filled out.

---
+++ 2 - Strings/1 - Strings & Characters/Program.cs
@@ -0,0 +1,31 @@
+﻿// char represents is character and uses single quotes
+char character = 'A';
+Console.WriteLine(character);
+
+// string represents a sequence of characters and uses double quotes
+string text = "Adventure";
+Console.WriteLine(text);
+
+// We can access a single character from the string using indexing
+char firstCharacter = text[0];
+Console.WriteLine(firstCharacter);
+
+// We can retrieve the ASCII code of a character
+int asciiCode = (int)firstCharacter;
+Console.WriteLine(asciiCode);
+
+// We can convert a char to string
+string firstCharacterAsString = firstCharacter.ToString();
+Console.WriteLine(firstCharacterAsString);
+
+// We get the length of the string by using Length property
+int textLength = text.Length;
+Console.WriteLine(textLength);
+
+// Initialize an empty string
+string emptyText = string.Empty;
+Console.WriteLine("[" + emptyText + "]");
+
+// Important: char and string are different types
+// char letter = "A";      // Error - we cannot auto-convert string to char
+// string text = 'C';      // Error - we cannot auto-convert char to string
---
+++ 2 - Strings/2 - String Operations/Program.cs
@@ -0,0 +1,47 @@
+﻿string text = "This is an example text.";
+string substring = "example";
+string untrimmedText = "  Example, Text!  ";
+
+Console.WriteLine("String Searching");
+Console.WriteLine(text.IndexOf(substring)); // Get index of first "example" in 'text'
+Console.WriteLine(text.LastIndexOf("e")); // Get last index of "e" in 'text'
+Console.WriteLine();
+
+Console.WriteLine("String Matching");
+Console.WriteLine(text.Contains("example")); // Check if "example" is in 'text'
+Console.WriteLine(text.StartsWith("This")); // Check if 'text' starts with "This"
+Console.WriteLine(text.EndsWith("text.")); // Check if 'text' ends with "text."
+Console.WriteLine();
+
+Console.WriteLine("String Modification");
+Console.WriteLine(text.Replace("text", "sentence")); // Replace word "text" with "sentence"
+Console.WriteLine(text.ToLower()); // Lowercase 'text'
+Console.WriteLine(text.ToUpper()); // Uppercase 'text'
+Console.WriteLine();
+
+Console.WriteLine("String Validation");
+Console.WriteLine(string.IsNullOrEmpty(string.Empty)); // Check if string is null or empty
+Console.WriteLine(string.IsNullOrWhiteSpace(string.Empty)); // Check if string is null, empty, or spaces only
+Console.WriteLine();
+
+Console.WriteLine("String Cleanup");
+Console.WriteLine("[" + untrimmedText.Trim() + "]"); // Trim spaces from start and end
+Console.WriteLine("[" + untrimmedText.TrimStart() + "]"); // Trim spaces from start
+Console.WriteLine("[" + untrimmedText.TrimEnd() + "]"); // Trim spaces from end
+Console.WriteLine();
+
+Console.WriteLine("String Comparison");
+Console.WriteLine(string.Equals(substring, "example")); // Check if two strings are equal
+Console.WriteLine();
+
+Console.WriteLine("String Splitting");
+string[] parts = text.Split(' '); // Split 'text' into parts based on space character
+for (int i = 0; i < parts.Length; i++) 
+{
+    Console.WriteLine(parts[i]); // Print each part
+}
+Console.WriteLine();
+
+Console.WriteLine("String Joining");
+Console.WriteLine(string.Join(" ", parts)); // Join parts we split back into text using space character
+Console.WriteLine();
