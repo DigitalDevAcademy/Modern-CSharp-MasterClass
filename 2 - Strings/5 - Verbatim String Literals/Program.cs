8fde373ef4530b9e028157fd97872e6f5fcc4c9f
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-12 14:47:40 -0400

    Strings content reorganized & updated
    Renaming some of the projects for better clarity. Cleaning up content from Raw String Literals and Verbatim Strings.

--- 2 - Strings/5 - Verbatim String Literals/Program.cs
+++ 2 - Strings/5 - Verbatim String Literals/Program.cs
@@ -1,21 +1,23 @@
 ﻿var fileDetails = string.Empty;
 var fileSize = "4 KB";
 
-// Auto escaping backslashes and creating new lines with verbatim strings
+// Auto escaping backslashes and creating new lines with verbatim string literals
 fileDetails = @"File Location: ""C:\Documents\File.txt""
-File Size: 4 KB";
+                File Size: 4 KB";
 Console.WriteLine(fileDetails);
 
 Console.WriteLine();
 
-/* Interpolated verbatim strings have the above mentioned benefits + interpolation
+/* Interpolated verbatim string literals have the above mentioned benefits + interpolation
  * These were introduced during C# 6 but after C# 8 .NET Core 3 
  * ($@ or @$) order does not matter
  */
 fileDetails = $@"File Location: ""C:\Documents\File.txt"" 
-File Size: {fileSize}";
+                 File Size: {fileSize}
+                 {{ No interpolated }}";
 Console.WriteLine(fileDetails);
 
 fileDetails = @$"File Location: ""C:\Documents\File.txt"" 
-File Size: {fileSize}";
+                 File Size: {fileSize}
+                 {{ No interpolated }}";
 Console.WriteLine(fileDetails);
--- 2 - Strings/6 - Raw String Literals/Program.cs
+++ 2 - Strings/6 - Raw String Literals/Program.cs
@@ -5,19 +5,19 @@ var fileSize = "4 KB";
  * Also creating new lines with raw string literals
  * Introduced during C# 11 .NET 7
  */
-Console.WriteLine("");
-fileDetails = $"""
-File Location: "C:\Documents\File.txt"
-File Size: 4 KB
-""";
+fileDetails = $""" 
+    File Location: "C:\Documents\File.txt"
+    File Size: 4 KB
+    """;
 Console.WriteLine(fileDetails);
 
 Console.WriteLine();
 
 // Interpolated raw strings literals have the above mentioned benefits + interpolation
-Console.WriteLine("Interpolated Raw String Literals");
-fileDetails = $"""
-File Location: "C:\Documents\File.txt"
-File Size: {fileSize}
-""";
+fileDetails = $$""""
+    File Location: "C:\Documents\File.txt"
+    File Size: {{{fileSize}}}
+    """ More file information """
+    { No interpolated }
+    """";
 Console.WriteLine(fileDetails);
