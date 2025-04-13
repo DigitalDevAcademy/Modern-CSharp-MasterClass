b0de3fd1f5e4270c9229581e2a944b35f45d9b10
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-12 21:29:59 -0400

    StringBuilder content updated
    Much better way of presenting StringBuilder properties and methods.

--- 2 - Strings/7 - StringBuilder/Program.cs
+++ 2 - Strings/7 - StringBuilder/Program.cs
@@ -2,24 +2,22 @@
 
 // This simple list could be something we get from a database or another source
 string[] reportParts = ["many", "parts", "of", "a", "report"];
-var immutableString = string.Empty;
 
-var stringBuilder = new StringBuilder();
+// Initializing a new StringBuilder
+StringBuilder sb = new();
 
-Console.WriteLine("String Concatenation");
-for(int i = 0; i < reportParts.Length; i++)
-{
-    var reportPart = reportParts[i];
-    immutableString += reportPart;
-    immutableString += "\n";
-}
-Console.WriteLine(immutableString);
+// This is how you set the capacity
+//StringBuilder sb = new(500);
 
-Console.WriteLine("StringBuilder");
+// This is how you set an initial string and set the capacity
+//StringBuilder sb = new("Some initial value", 500);
+
+// Iterating through parts of the report to build the full version
 for (int i = 0; i < reportParts.Length; i++)
 {
-    var reportPart = reportParts[i];
-    stringBuilder.AppendLine(reportPart);
-
+    sb.AppendLine(reportParts[i]);
+    // finalReport += reportParts[i] // This would create multiple copies
 }
-Console.WriteLine(stringBuilder.ToString());
+
+// Printing the full version of the report
+Console.WriteLine(sb.ToString());
