2b06a4f1c0a57f3154d0e44cc6206805539b5907
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-15 14:36:15 -0400

    Value vs Reference Types, moved, content created
    Moving Value vs Reference Types after reorganizing course structure for better convenience and clarity. Adding new contents to it.

---
+++ 5 - Value vs Reference Types/1 - Stack vs Heap/Program.cs
@@ -0,0 +1,30 @@
+﻿// Initializing a struct and a class with only one field
+UserStruct userStruct = new() { Id = 1 };
+UserClass userClass = new() { Id = 1 };
+
+// Printing both values before changing them
+Console.WriteLine($"UserStruct.Id: {userStruct.Id} | UserClass.Id: {userClass.Id}");
+
+ChangeUserId(userStruct, userClass);
+
+// Printing both values after changing them
+Console.WriteLine($"UserStruct.Id: {userStruct.Id} | UserClass.Id: {userClass.Id}");
+
+// Modify both values to see the difference between stack vs heap
+void ChangeUserId(UserStruct userStruct, UserClass userClass)
+{
+    userStruct.Id = 2;
+    userClass.Id = 2;
+}
+
+// Struct is a value type, so it is stored on the stack
+struct UserStruct
+{
+    public int Id { get; set; }
+}
+
+// Class is a reference type, so it is stored on the heap
+class UserClass
+{
+    public int Id { get; set; }
+}
---
+++ 5 - Value vs Reference Types/2 - Out vs Ref vs In/Program.cs
@@ -0,0 +1,29 @@
+﻿User user1 = new(1);
+ChangeUserWithRef(ref user1);
+void ChangeUserWithRef(ref User user)
+{
+    user.Id = 2;
+}
+Console.WriteLine($"User1 Id is {user1.Id}");
+
+//var User user2; <-- We no longer have to declare a separate variable and can do it inline since C# 7.0
+CreateUserWithOut(out User user2);
+void CreateUserWithOut(out User user)
+{
+    user = new(2);
+}
+Console.WriteLine($"User2 Id is {user2.Id}");
+
+User user3 = new(1);
+AccessUserWithIn(in user3); // in keyword introduced in C# 7.2
+void AccessUserWithIn(in User user)
+{
+    //user = new(1); <-- Results in compile-time error since 'user' is read-only
+    //user.Id = 2;   <-- Results in compile-time error since 'user' is read-only
+    Console.WriteLine($"User3 Id is {user.Id}"); // <-- We can read user but cannot modify it
+}
+
+struct User(int id)
+{
+    public int Id { get; set; } = id;
+}
