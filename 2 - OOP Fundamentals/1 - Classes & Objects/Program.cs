2b7918bff7324ea87259706817f5826c915b546e
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-18 22:44:35 -0400

    Classes & Objects content updated
    Adding required content to the Classes & Objects project to replace default code.

--- 2 - OOP Fundamentals/1 - Classes & Objects/Program.cs
+++ 2 - OOP Fundamentals/1 - Classes & Objects/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Instantiating a new instance of a class using target-typed new expression form C# 9 .NET 5
+Car car1 = new();
+// You can also instantiate this way, but unless you have a good reason to do so use target-typed new expression
+// Car car1 = new Car();
+// var car1 = new Car();
+
+// You can instantiate many instances of a class
+Car car2 = new();
+
+// Instantiate another instance from the same class to futher illustrate the above point
+Car car3 = new();
+
+// Create an empty class with public access modifier, no modifiers and no class members
+public class Car
+{
+    // Class body
+}
