784ab8b4e1af3916861008a5237608d356ec8b2b
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-20 19:13:17 -0400

    Fields content updated
    Adding required content to the Fields project to replace default code.

--- 2 - OOP Fundamentals/02 - Fields/Program.cs
+++ 2 - OOP Fundamentals/02 - Fields/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Instantiate frist Employee object
+var employee1 = new Employee();
+employee1.name = "SomeEmployee1";
+// employee1.salary = 100000; // Cannot access salary, it is a private field
+Console.WriteLine($"Employee 1 name is {employee1.name}.");
+
+// Instantiate second Employee object
+var employee2 = new Employee();
+employee2.name = "SomeEmployee2";
+// employee2.salary = 100000; // Cannot access salary, it is a private field
+Console.WriteLine($"Employee 2 name is {employee1.name}.");
+
+public class Employee
+{
+    public string name = string.Empty;
+
+    private double salary = 0; // Cannot access or modify a private field anywhere else except the class itself
+}
