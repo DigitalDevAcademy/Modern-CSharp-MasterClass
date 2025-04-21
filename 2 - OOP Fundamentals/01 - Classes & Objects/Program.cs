82a3e9305215b96af82c08178fd7754ed5560d40
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-21 17:34:52 -0400

    Classes & Objects content updated
    Changing variable names for object instances to represent real-world objects for better clarity.

--- 2 - OOP Fundamentals/01 - Classes & Objects/Program.cs
+++ 2 - OOP Fundamentals/01 - Classes & Objects/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Instantiating a new instance of a class using target-typed new expression form C# 9 .NET 5
-Car car1 = new();
+Car toyotaCorolla = new();
 // You can also instantiate this way, but unless you have a good reason to do so use target-typed new expression
-// Car car1 = new Car();
-// var car1 = new Car();
+// Car toyotaCorolla = new Car();
+// var toyotaCorolla = new Car();
 
 // You can instantiate many instances of a class
-Car car2 = new();
+Car hondaCivic = new();
 
 // Instantiate another instance from the same class to futher illustrate the above point
-Car car3 = new();
+Car fordMustang = new();
 
 // Create an empty class with public access modifier, no modifiers and no class members
 public class Car
