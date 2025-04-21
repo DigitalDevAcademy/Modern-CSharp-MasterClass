d6d6ef7f9f8a4b62683faa569c1836581e356894
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-21 17:53:59 -0400

    Revert "Classes & Objects content updated"
    This reverts commit 82a3e9305215b96af82c08178fd7754ed5560d40.

--- 2 - OOP Fundamentals/01 - Classes & Objects/Program.cs
+++ 2 - OOP Fundamentals/01 - Classes & Objects/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Instantiating a new instance of a class using target-typed new expression form C# 9 .NET 5
-Car toyotaCorolla = new();
+Car car1 = new();
 // You can also instantiate this way, but unless you have a good reason to do so use target-typed new expression
-// Car toyotaCorolla = new Car();
-// var toyotaCorolla = new Car();
+// Car car1 = new Car();
+// var car1 = new Car();
 
 // You can instantiate many instances of a class
-Car hondaCivic = new();
+Car car2 = new();
 
 // Instantiate another instance from the same class to futher illustrate the above point
-Car fordMustang = new();
+Car car3 = new();
 
 // Create an empty class with public access modifier, no modifiers and no class members
 public class Car
