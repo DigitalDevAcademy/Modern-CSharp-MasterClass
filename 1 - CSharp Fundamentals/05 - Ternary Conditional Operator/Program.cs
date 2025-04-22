46f6c1f3c734fff2574f9aa2f7dcec050d4c85c1
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-22 19:37:55 -0400

    Ternary Conditional Operator content updated
    Adding all of the required content, demonstrating on two simple examples how to use the Ternary Conditional Operator.

--- 1 - CSharp Fundamentals/05 - Ternary Conditional Operator/Program.cs
+++ 1 - CSharp Fundamentals/05 - Ternary Conditional Operator/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Declaring a variable representing a test score - image we have many of them from all students
+int testScore = 100;
+
+// We then could retrieve those scores and perform a conditional check to see if the students passed or failed
+string result = testScore >= 80 ? "Pass" : "Fail";
+
+// Notice how the result is of type string because Operand 2 and Operand 3 are of type string
+Console.WriteLine(result);
+
+// Now let's declare another variable representing an item quantity
+int itemQuantity = 5;
+
+// We can check and see if there are 5 or more items then provide a 25% discount, otherwise provide a 5% discount
+decimal discount = itemQuantity >= 5 ? 0.25m : 0.05m;
+
+// Once again notice how discount is of type decimal becasue both Operand 2 and 3 are of type decimal
+Console.WriteLine(discount);
