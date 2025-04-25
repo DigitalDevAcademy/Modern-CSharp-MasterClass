9f3ba921dae782c88180787c167098da55e2fa00
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-24 22:46:47 -0400

    Classes & Objects updated
    Updating content for each OOP Fundamentals lectures starting with Classes & Objects project to contain specific examples as needed instead of creating a theme of examples which could make things more confusing for students.

--- 2 - OOP Fundamentals/01 - Classes & Objects/Program.cs
+++ 2 - OOP Fundamentals/01 - Classes & Objects/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Instantiating a new instance of a class using target-typed new expression form C# 9 .NET 5
-Car car1 = new();
+Employee employee1 = new();
 // You can also instantiate this way, but unless you have a good reason to do so use target-typed new expression
-// Car car1 = new Car();
-// var car1 = new Car();
+// Employee employee1 = new Employee();
+// var employee1 = new Employee();
 
 // You can instantiate many instances of a class, I am just instantiating one more to keep things simple
-Car car2 = new();
+Employee employee2 = new();
 
 // Create an empty class with public access modifier, no modifiers and no class members
-public class Car
+public class Employee
 {
     // Class body
 }
