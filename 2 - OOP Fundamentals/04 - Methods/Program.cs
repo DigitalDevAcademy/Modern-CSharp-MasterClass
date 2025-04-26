359278aeaf4b8d3aa3fed1011678c8d5a5f5534e
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-26 15:29:33 -0400

    OOP Fundamentals reorganized, moved Methods
    Moved Methods project right after Properties project. Structure is much clearer this way.

---
+++ 2 - OOP Fundamentals/04 - Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿Car myCar = new();
+myCar.Drive();
+myCar.Drive();
+myCar.Drive();
+myCar.Drive();
+myCar.Drive();
+myCar.Refuel(-100);
+myCar.Refuel(101);
+myCar.Drive();
+myCar.Drive();
+myCar.Drive();
+myCar.Drive();
+myCar.Drive();
+myCar.Refuel(100);
+myCar.Drive();
+
+public class Car
+{
+    public int CurrentFuel { get; set; } = 100;
+
+    // Void method does not return anything, also parameterless meaning it does not take in any parameters
+    public void Drive()
+    {
+        if (CurrentFuel > 0)
+        {
+            Console.WriteLine("Car is moving.");
+            CurrentFuel -= 25;
+        }
+        else
+        {
+            Console.WriteLine("Car doesn't have enough fuel. Please refuel!");
+        }
+    }
+
+    // Method returning value of type bool and taking in one parameter of type int
+    public bool Refuel(int newFuel)
+    {
+        if (newFuel < 0)
+        {
+            // In real-life this would be an exception, but for now we are printing a message
+            Console.WriteLine("No fuel was added.");
+            return false;
+        }
+        
+        if (CurrentFuel + newFuel > 100)
+        {
+            // If the sum of incoming fuel and current fuel go above 100 we only keep 100 and discard the rest and print a message
+            CurrentFuel = 100;
+            Console.WriteLine("The fuel tank is over capacity.");
+            return true;
+        }
+        
+        // If we are within capacity then keep adding incoming fuel to our current fuel level
+        CurrentFuel += newFuel;
+        return true;
+    }
+}
