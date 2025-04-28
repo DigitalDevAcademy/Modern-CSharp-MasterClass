4c0b9e8477c937c524a1fbc5095b7f2bb27ba468
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-27 21:35:56 -0400

    Constructor content created
    Helpful example added with two constructors showcasing the difference between parameterless and parameterized constructors.

--- 2 - OOP Fundamentals/05 - Constructor/Program.cs
+++ 2 - OOP Fundamentals/05 - Constructor/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Robot robotOne = new();
+Console.WriteLine($"Robot Info: {robotOne.Name}, {robotOne.Map}");
+
+Robot robotTwo = new("New Robot", "New Map");
+Console.WriteLine($"Robot Info: {robotTwo.Name}, {robotTwo.Map}");
+
+public class Robot
+{
+    public string Name { get; init; }
+
+    public string Map { get; set; }
+
+    // Parameterless constructor
+    public Robot()
+    {
+        Name = "Default Robot";
+        Map = string.Empty;
+    }
+
+    // Constructor with two params and basic validation logic
+    public Robot(string name, string map)
+    {
+        Name = string.IsNullOrWhiteSpace(name) ? "Default Robot" : name;
+        Map = map;
+    }
+}
