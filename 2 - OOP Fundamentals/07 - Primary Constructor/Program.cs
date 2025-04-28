f6d0fda33a91e237c7bae7fb4f5fa386aeb0b287
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-28 14:18:31 -0400

    This Keyword project added, reorganized
    Adding This Keyword project as the explanation is needed before moving ahead.

---
+++ 2 - OOP Fundamentals/07 - Primary Constructor/Program.cs
@@ -0,0 +1,14 @@
+﻿Robot robotOne = new();
+Console.WriteLine($"Robot Info: {robotOne.Name}, {robotOne.Map}");
+
+Robot robotTwo = new("New Robot", "New Map");
+Console.WriteLine($"Robot Info: {robotTwo.Name}, {robotTwo.Map}");
+
+public class Robot(string name, string map)
+{
+    public Robot() : this("Unnamed Robot", "") { }
+    
+    public string Name { get; init; } = name;
+
+    public string Map { get; set; } = map;
+}
