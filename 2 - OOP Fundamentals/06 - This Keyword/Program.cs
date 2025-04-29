08cc8c7d9774e1aa5eae532b1c8502d4b7eb7a05
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-28 20:20:40 -0400

    This Keyword content added, constructor chaining
    Adding an example for constructor chaining. Method extensions we are not covering in this section and name conflict resolution is explained in the slides - since it is not recommended, no example code is going to be added for it.

--- 2 - OOP Fundamentals/06 - This Keyword/Program.cs
+++ 2 - OOP Fundamentals/06 - This Keyword/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Manager manager;
+
+manager = new();
+Console.WriteLine($"Manager Info: {manager.FullName}, {manager.Age}");
+
+manager = new("John Doe");
+Console.WriteLine($"Manager Info: {manager.FullName}, {manager.Age}");
+
+manager = new("John Doe", 40);
+Console.WriteLine($"Manager Info: {manager.FullName}, {manager.Age}");
+
+public class Manager
+{
+    public string FullName { get; set; }
+
+    public int Age { get; set; }
+
+    // This is constructor chaining that can be used with the this keyword
+    public Manager() : this("Default Name")
+    {
+    }
+
+    // Showcasing multiple constructor chaining using the this keyword
+    public Manager(string fullName) : this(fullName, 30)
+    {
+    }
+
+    public Manager(string fullName, int age)
+    {
+        FullName = fullName;
+        Age = age;
+    }
+}
