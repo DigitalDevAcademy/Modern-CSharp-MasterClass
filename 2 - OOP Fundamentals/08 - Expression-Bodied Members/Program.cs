0e18dc264d71400111b6cdbc5a687e1397aeac32
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-29 18:38:13 -0400

    Expression-Bodied Members demo all class members
    Showcasing how to turn all class members into expression-bodied members to write cleaner code when only a one-liner is needed.

--- 2 - OOP Fundamentals/08 - Expression-Bodied Members/Program.cs
+++ 2 - OOP Fundamentals/08 - Expression-Bodied Members/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿SomeClass someClass = new();
+Console.WriteLine(someClass.someProperty);
+someClass.someProperty = "New Value";
+Console.WriteLine(someClass.someProperty);
+Console.WriteLine(someClass.someReadOnlyProperty1);
+Console.WriteLine(someClass.someReadOnlyProperty2);
+Console.WriteLine(someClass.someMethod());
+
+public class SomeClass
+{
+    public SomeClass() => someProperty = "Default Value";
+    
+    private string _someField = string.Empty; 
+
+    public string someProperty
+    {
+        get => _someField;
+        set => _someField = value;
+    }
+
+    public string someReadOnlyProperty1 => "Some Read-Only Value";
+
+    public string someReadOnlyProperty2 { get => "Another Read-Only Value"; }
+
+    public string someMethod() => $"{someReadOnlyProperty1} {someReadOnlyProperty2}";
+}
