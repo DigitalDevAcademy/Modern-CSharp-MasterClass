1a3a4a8e353f67783844b6f0354830216250d009
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-29 15:57:11 -0400

    Primary Constructor content updated, new method added
    Adding a new instance method to demonstrate how using initialized properties instead of primary constructor parameters is the best practice. This way avoiding double backing field issues.

--- 2 - OOP Fundamentals/07 - Primary Constructor/Program.cs
+++ 2 - OOP Fundamentals/07 - Primary Constructor/Program.cs
@@ -1,14 +1,30 @@
 ﻿Robot robotOne = new();
-Console.WriteLine($"Robot Info: {robotOne.Name}, {robotOne.Map}");
+robotOne.PrintRobotInfo();
 
 Robot robotTwo = new("New Robot", "New Map");
-Console.WriteLine($"Robot Info: {robotTwo.Name}, {robotTwo.Map}");
+robotTwo.PrintRobotInfo();
 
 public class Robot(string name, string map)
 {
     public Robot() : this("Default Robot", string.Empty) { }
     
+    /* 
+     * Only use primary constructor parameters to initialize properties but
+     * do not use them elsewhere in the class becasue it could create a double
+     * backing field problem which could be confusing to troubleshoot
+     */
     public string Name { get; init; } = name;
 
     public string Map { get; set; } = map;
+
+
+    public void PrintRobotInfo() 
+    {
+        /* 
+         * Here I am using initialized properties instead of primary constructor parameters
+         * Even though I could use primary constructor parameters I am not using them 
+         * to avoid double backing field issues
+         */
+        Console.WriteLine($"Name: {Name}, Map: {Map}");
+    }
 }
