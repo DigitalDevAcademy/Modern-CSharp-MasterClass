e6705f327d6b386d587da695013c9cff0c7f5e83
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-04-30 15:06:03 -0400

    Static vs Instance Members renamed, content added
    Renaming Static vs Non-Static to Static vs Instance Members as it better describes the concepts. Also adding content and comments for the example.

---
+++ 2 - OOP Fundamentals/09 - Static vs Instance Members/Program.cs
@@ -0,0 +1,16 @@
+﻿AdditionHelper.Add(10);
+Console.WriteLine(AdditionHelper.Result);
+
+AdditionHelper.Add(10);
+Console.WriteLine(AdditionHelper.Result);
+
+// Static class cannot have any instance members only static members
+public static class AdditionHelper
+{
+    // We can have static fields and properties, but I am only using a property to keep the example concise
+    public static int Result { get; set; }
+
+    static AdditionHelper() => Result = 0;
+
+    public static void Add(int number) => Result += number;
+}
