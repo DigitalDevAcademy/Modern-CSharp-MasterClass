7e6f6233e68239b55181be948f9e3df88b89058a
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-01 17:05:56 -0400

    Readonly vs Const Fields content updated
    Adding an example showcasing const fields, readonly fields, static readonly fields and local constants.

--- 2 - OOP Fundamentals/10 - Readonly vs Const Fields/Program.cs
+++ 2 - OOP Fundamentals/10 - Readonly vs Const Fields/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿SomeDependency dependency = new();  
+SomeClass someClass = new(dependency);
+ 
+someClass.SomeMethod();
+someClass.PrintLocalConstant();
+// someClass.dependency = new SomeDependency(); // A readonly field cannot be assigned to after initialization
+
+Console.WriteLine($"Static Readonly Field Value: {SomeClass.someUniqueId}");
+Console.WriteLine($"Const Field Value: {SomeClass.someConstant}");
+
+public class SomeClass
+{
+    // Declaring a readonly field - can hold any type and be assigned directly or inside constructor
+     public readonly SomeDependency dependency;
+
+     /* 
+      * Declaring static readonly field - can hold any type and be evaluated during runtime and be assigned 
+      * directly or inside static constructor
+      */ 
+     public static readonly Guid someUniqueId = Guid.NewGuid();
+
+     // Declaring a const field - can only hold primitives and string and only be assigned directly
+     public const string someConstant = "Constant Value";
+
+    // Perfomring IoC (Inversion of Control) using constructor injection
+     public SomeClass(SomeDependency dependency)
+     {
+          this.dependency = dependency;
+     }
+     
+     // SomeMethod using method from a dependency
+     public void SomeMethod() => dependency.DoWork();
+     
+     public void PrintLocalConstant()
+     {
+        // Declaring a local constant, const within a method
+        const string someValue = "SomeText";
+        Console.WriteLine($"Local Constant Value: {someValue}");
+     }    
+}
+
+public class SomeDependency
+{
+    public void DoWork() => Console.WriteLine("Work completed.");
+}
