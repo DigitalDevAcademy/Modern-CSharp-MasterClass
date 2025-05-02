6cb534de89f6fe5523b98f76637a811d2962dcdb
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-02 12:16:28 -0400

    Required Modifier content added
    Creating a class with required properties and some which are not to showcase the difference. Not using required for public fields because public fields are not recommended in general.

--- 2 - OOP Fundamentals/11 - Required Modifier/Program.cs
+++ 2 - OOP Fundamentals/11 - Required Modifier/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Product productOne = new() { Name = "Laptop", Price = 1050.55m, Category = "Technology" };
+Console.WriteLine(productOne.GetInfo());
+
+Product productTwo = new() { Name = "Battery Pack", Price = 15.99m };
+Console.WriteLine(productTwo.GetInfo());
+
+public class Product
+{
+    // Object initializer is required to set this property
+    public required string Name { get; init; }
+
+    // Object initializer is required to set this property
+    public required decimal Price { get; init; }
+
+    public string Category { get; set; } = "Default";
+
+    public string GetInfo() => $"Product Info: {Name} {Price} {Category}";
+}
