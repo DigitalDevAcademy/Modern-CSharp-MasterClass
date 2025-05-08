2c4bc766124354f288651f879b8e6489b1baafe8
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-08 11:42:30 -0400

    Explicit Parameterless Constructor renamed & added
    Renaming the project to its more technically correct name and adding required code to showcase the feature.

---
+++ 3 - Structs & Enums/04 - Explicit Parameterless Constructor/Program.cs
@@ -0,0 +1,17 @@
+﻿Point point1 = new();
+Console.WriteLine(point1.GetInfo());
+
+public readonly struct Point
+{
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    // C# 10 .NET 6 introduced explicit parameterless constructor for structs
+    public Point()
+    {
+        X = 10;
+        Y = 10;
+    }
+
+    public string GetInfo() => $"X is {X}, Y is {Y}.";
+}
