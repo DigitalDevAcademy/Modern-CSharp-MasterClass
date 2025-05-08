d272cc63cdd306b998f64982dd91905ef6910f46
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-08 14:35:26 -0400

    Primary constructor example added
    Adding a simple example demonstrating primary constructors for structs.

--- 3 - Structs & Enums/06 - Primary Constructor/Program.cs
+++ 3 - Structs & Enums/06 - Primary Constructor/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Point point = new(10, 10);
+Console.WriteLine(point.GetInfo());
+
+// C# 12 .NET 8 introduced primary constructor for structs (and few other types)
+public readonly struct Point(int x, int y)
+{
+    public int X { get; init; } = x;
+    public int Y { get; init; } = y;
+
+    public string GetInfo() => $"X is {X}, Y is {Y}.";
+}
