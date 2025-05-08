028446d3244f7d18f016aa8643d0cdab74cf18ce
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-07 20:56:47 -0400

    Reaodnly Structs some cleanup
    Using GetInfo and cleaning/adding comments about using readonly fields on readonly structs.

--- 3 - Structs & Enums/02 - Readonly Structs/Program.cs
+++ 3 - Structs & Enums/02 - Readonly Structs/Program.cs
@@ -1,15 +1,24 @@
 ﻿Point point1 = new(10, 10);
-Console.WriteLine($"Point1 X: {point1.X}, Y: {point1.Y}.");
+Console.WriteLine(point1.GetInfo());
 // Cannot change immutable property values
 // point1.X = 20;
 // point1.Y = 20;
 
-/* 
- * Readonly structs available starting C# 7.2 .NET Core 2.1/Framework 4.7.2
- * We can use primary constructor for structs as well since C# 12 .NET 8
- */
-public readonly struct Point(int x, int y)
+// Readonly structs available starting C# 7.2 .NET Core 2.1/Framework 4.7.2
+public readonly struct Point
 {
-    public int X { get; init; } = x;
-    public int Y { get; init; } = y;
+    public int X { get; init; }
+    public int Y { get; init; }
+
+    // We can also use immutable fields
+    // public readonly int X;
+    // public readonly int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public string GetInfo() => $"X is {X}, Y is {Y}.";
 }
