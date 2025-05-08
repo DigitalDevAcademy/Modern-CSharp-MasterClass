5dc207ae1ba546679ea949e1820cd437c00f6693
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-08 15:52:30 -0400

    Primitives As Structs cleaning up example
    String is presented as a primitive in some places and not others. Including it as part of primitives makes things confusing. It is specially treated class within C# but much better to present it separately from primitives.

--- 3 - Structs & Enums/07 - Primitives As Structs/Program.cs
+++ 3 - Structs & Enums/07 - Primitives As Structs/Program.cs
@@ -5,7 +5,6 @@
 */
 #pragma warning disable CS0219
 
-// Structs - value types
 System.Int32 totalProducts = 50; // int
 System.Int64 totalWebsiteVisits = 1000000000000; // long
 System.Single playerViewDistance = 1350.75f; // float
@@ -13,9 +12,6 @@ System.Double calculationResult = 15009090.132453849519; // double
 System.Decimal accountBalance = 100500.1050m; // decimal
 System.Boolean isUserLoggedIn = true; // bool
 System.Char studentGrade = 'A'; // char
-
-// Class - reference type - string is also a primitive but only one which is not a value type
-System.String welcomeText = "Hello World!"; // string
 
 #pragma warning restore CS0219
 
