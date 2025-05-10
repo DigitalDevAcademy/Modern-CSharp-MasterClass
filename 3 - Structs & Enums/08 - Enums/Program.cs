a4bc3912dd5877be0048b4f4fd8c2eb1761e4cb6
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-09 20:46:05 -0400

    Enums updating example
    Simplifying example and only using switch statement to keep things clean.

--- 3 - Structs & Enums/08 - Enums/Program.cs
+++ 3 - Structs & Enums/08 - Enums/Program.cs
@@ -1,30 +1,28 @@
-﻿string paymentType2 = "Credit";
-Console.WriteLine(paymentType2);
-
-int paymentType3 = 11;
-Console.WriteLine(paymentType3);
-
-PaymentType paymentType1 = PaymentType.Credit;
-Console.WriteLine(paymentType1);
-Console.WriteLine((int)paymentType1);
+﻿ProcessPayment(50, PaymentType.Crypto);
 
 // Provides us with compile-time safety and keeps code refactor-friendly
-if (paymentType1 == PaymentType.Credit)
+void ProcessPayment(decimal amount, PaymentType paymentType)
 {
-    Console.WriteLine("Credit payment");
-}
-
-switch(paymentType1)
-{
-    case PaymentType.Credit:
-        Console.WriteLine("Credit payment");
-        break;
+    switch(paymentType)
+    {
+        case PaymentType.Cash:
+            Console.WriteLine($"Processing ${amount} through in-person exchange.");
+            break;
+        case PaymentType.Bank:
+            Console.WriteLine($"Processing ${amount} through bank transfer (ACH or Wire).");
+            break;
+        case PaymentType.Crypto:
+            Console.WriteLine($"Processing ${amount} through blockchain transaction.");
+            break;
+        default:
+            Console.WriteLine("Unknown payment type.");
+            break;
+    }
 }
 
 enum PaymentType
 {
-    None = 0, // Automatically assigned the value zero (since it has no initializer and is the first enum member)
     Cash,
-    Crypto = 10, // Explicitly setting to 10
-    Credit // Auto-increments by one from the previous value
+    Bank,
+    Crypto
 }
