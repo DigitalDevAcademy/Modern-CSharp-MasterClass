be2e362c71133bd35fc6ee9fc6794dd68585bece
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-15 17:18:00 -0400

    Reorganizing, adding System.Object Class project
    Showcasing how to override ToString method, reorganizing projects, some name cleanup.

---
+++ 4 - Type Hierarchy & Behavior/01 - System.Object Class/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * int, string, custom class Person, custom struct Point all implicitly derive from System.Object
+ * which is why we are able to upcast from our types to System.Object
+ */
+object number = 10;
+object text = "Hello World";
+object person = new Person("John", "Doe");
+object point = new Point(10, 10);
+
+Console.WriteLine(number);
+Console.WriteLine(text);
+Console.WriteLine(person);
+Console.WriteLine(point);
+
+public class Person(string firstName, string lastName)
+{
+    public string FirstName { get; init; } = firstName;
+
+    public string LastName { get; init; } = lastName;
+    
+    public override string ToString() => $"FirstName {FirstName} and LastName {LastName}.";
+}
+
+public struct Point(int x, int y)
+{
+    public int X { get; init; } = x;
+    public int Y { get; init; } = y;
+}
