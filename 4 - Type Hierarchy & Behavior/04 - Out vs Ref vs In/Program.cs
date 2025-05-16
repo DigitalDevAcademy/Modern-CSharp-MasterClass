2f363b725ebf61eb2931b60f67e053e15ca49ecc
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-16 14:26:17 -0400

    Out vs Ref vs In cleaned and clarified
    Changing to use built-in readonly struct instead of using custom struct. Cleaning up syntax and comments.

--- 4 - Type Hierarchy & Behavior/04 - Out vs Ref vs In/Program.cs
+++ 4 - Type Hierarchy & Behavior/04 - Out vs Ref vs In/Program.cs
@@ -1,29 +1,14 @@
-﻿User user1 = new(1);
-ChangeUserWithRef(ref user1);
-void ChangeUserWithRef(ref User user)
-{
-    user.Id = 2;
-}
-Console.WriteLine($"User1 Id is {user1.Id}");
+﻿int firstNumber = 1;
+ChangeUsingRef(ref firstNumber);
+void ChangeUsingRef(ref int number) => number = 2;
+Console.WriteLine(firstNumber);
 
-//var User user2; <-- We no longer have to declare a separate variable and can do it inline since C# 7.0
-CreateUserWithOut(out User user2);
-void CreateUserWithOut(out User user)
-{
-    user = new(2);
-}
-Console.WriteLine($"User2 Id is {user2.Id}");
+// Starting with C# 7.0 you can declare out variables inline
+ChangeUsingOut(out int secondNumber);
+void ChangeUsingOut(out int number) => number = 2;
+Console.WriteLine(secondNumber);
 
-User user3 = new(1);
-AccessUserWithIn(in user3); // in keyword introduced in C# 7.2
-void AccessUserWithIn(in User user)
-{
-    //user = new(1); <-- Results in compile-time error since 'user' is read-only
-    //user.Id = 2;   <-- Results in compile-time error since 'user' is read-only
-    Console.WriteLine($"User3 Id is {user.Id}"); // <-- We can read user but cannot modify it
-}
-
-struct User(int id)
-{
-    public int Id { get; set; } = id;
-}
+int thirdNumber = 1;
+// Starting with C# 7.2 you can declare method params as read-only, recommended for value-types only
+AccessUsingIn(in thirdNumber);
+void AccessUsingIn(in int number) => Console.WriteLine(number); // number = 2; // <-- Results in compile-time error
