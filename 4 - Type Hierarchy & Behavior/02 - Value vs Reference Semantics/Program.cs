1871f4f92fb8a012e76efa0cc3b5ff56536ee1a8
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-05-17 15:09:52 -0400

    Value vs Reference Type splitting into multiple
    Breaking the project apart to make it more byte-sized and easier for students to watch and understand.

--- 4 - Type Hierarchy & Behavior/02 - Value vs Reference Semantics/Program.cs
+++ 4 - Type Hierarchy & Behavior/02 - Value vs Reference Semantics/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Initializing a struct and a class with only one field
-// Value vs reference semantics - example 1
 UserStruct userStruct = new(30);
 UserClass userClass = new(30);
 
@@ -14,27 +13,6 @@ anotherUserClass.Age++;
 
 Console.WriteLine(anotherUserClass.Age);
 Console.WriteLine(userClass.Age);
-
-// Value vs reference semantics - example 2
-// UserStruct userStruct = new(30);
-// UserClass userClass = new(30);
-
-// // Printing both values before changing them
-// Console.WriteLine(userStruct.Age);
-// Console.WriteLine(userClass.Age);
-
-// ChangeUserAge(userStruct, userClass);
-
-// // Printing both values after changing them
-// Console.WriteLine(userStruct.Age);
-// Console.WriteLine(userClass.Age);
-
-// // Modify both values to see the difference between stack vs heap
-// void ChangeUserAge(UserStruct userStruct, UserClass userClass)
-// {
-//     userStruct.Age = 35;
-//     userClass.Age = 35;
-// }
 
 // Struct is a value type
 struct UserStruct(int age)
