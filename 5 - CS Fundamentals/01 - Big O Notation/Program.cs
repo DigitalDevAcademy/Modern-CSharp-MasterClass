574bf7ddf048476edc541d63f6ae97020d241c08
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-01 17:09:19 -0400

    Big O Notation lecture - updating comments
    Updating comments to make them even more helpful.

--- 5 - CS Fundamentals/01 - Big O Notation/Program.cs
+++ 5 - CS Fundamentals/01 - Big O Notation/Program.cs
@@ -1,4 +1,6 @@
-﻿// O(1) time - both declaration and printing of the value are constant operations
+﻿// All space complexities here are O(1). In real scenarios, space use would likely be higher during processing.
+
+// O(1) time - both declaration and printing of the value are constant operations
 var number = 10;
 Console.WriteLine(number);
 
@@ -9,7 +11,7 @@ for (int i = 0; i < itemLimit; i++)
     Console.WriteLine($"Processing item{i}.");
 }
 
-// O(n + m) – number of operations grows at the same rate as 'firstRunLimit' and 'secondRunLimit' - sequentially
+// O(n + m) time – number of operations grows at the same rate as 'firstRunLimit' and 'secondRunLimit' - sequentially
 var firstRunLimit = 10;
 for (int i = 0; i < firstRunLimit; i++)
 {
@@ -22,7 +24,7 @@ for (int i = 0; i < secondRunLimit; i++)
     Console.WriteLine($"Second run of all items - run({i})");
 }
 
-/// For each row, we go through all columns, making this O(n * m) – or O(r * c) to keep it clear
+// For each row, we go through all columns, making this O(n * m) time – or O(r * c) time to keep it clear
 var totalColumns = 10;
 var totalRows = 5;
 for (int row = 0; row < totalRows; row++)
@@ -32,6 +34,3 @@ for (int row = 0; row < totalRows; row++)
         Console.WriteLine($"Processing  Row{row} - Col{col}.");
     }
 }
-
-
-
