fee7a8ac1b946b7b79f53ea0d69f5fcbacddb84e
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-02 13:43:12 -0400

    Array Structure lecture material ready
    Will be showcasing declaration, access and that value/reference types can both be used.

---
+++ 6 - Arrays/01 - Array Structure/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array = new int[10];
+array[0] = 100;
+array[9] = 900;
+
+Console.WriteLine(array[0]);
+Console.WriteLine(array[9]);
+Console.WriteLine(array[array.Length - 1]);
+Console.WriteLine(array[1]); // This shows the default value
+
+// array[-1] = 1; // Will get an IndexOutOfRangeException
+// array[10] = 1; // Will get an IndexOutOfRangeException
+
+int[] initArray = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine(initArray[0]);
+Console.WriteLine(initArray[4]);
+
+Element[] initClassArray = { new Element(10), new Element(20), new Element(50) };
+Console.WriteLine(initClassArray[0].Value);
+Console.WriteLine(initClassArray[1].Value);
+Console.WriteLine(initClassArray[2].Value);
+
+public class Element(int element) { public int Value { get; init; } = element; }
+
+
+
