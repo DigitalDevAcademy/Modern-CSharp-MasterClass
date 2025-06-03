1d89f981407b29b9baee35508ff8c29107105645
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-03 19:59:14 -0400

    Array Structure combining lecture and adding notes
    Combining collection expressions into the lecture as it makes sense to do so, declaration and initialization are core part of the array structure so talking about it separately didn't make sense. Spread operator will be kept separate as it is an extended functionality. Adding time complexities moving forward after CS fundamentals covered.

--- 6 - Arrays/01 - Array Structure/Program.cs
+++ 6 - Arrays/01 - Array Structure/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] array = new int[10];
+﻿// All index-based access is O(1) time and space
+
+int[] array = new int[10];
 array[0] = 100;
 array[9] = 900;
 
@@ -10,12 +12,13 @@ Console.WriteLine(array[1]); // This shows the default value
 // array[-1] = 1; // Will get an IndexOutOfRangeException
 // array[10] = 1; // Will get an IndexOutOfRangeException
 
-int[] initArray = { 1, 2, 3, 4, 5 };
+// Using collection expressions available since C# 12 .NET 8
+int[] initArray = [1, 2, 3, 4, 5];
 
 Console.WriteLine(initArray[0]);
 Console.WriteLine(initArray[4]);
 
-Element[] initClassArray = { new Element(10), new Element(20), new Element(50) };
+Element[] initClassArray = [ new Element(10), new Element(20), new Element(50) ];
 Console.WriteLine(initClassArray[0].Value);
 Console.WriteLine(initClassArray[1].Value);
 Console.WriteLine(initClassArray[2].Value);
