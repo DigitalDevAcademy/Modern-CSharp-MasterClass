f2c1655a6cb188db49a5ff7ee12c8d0314023d3f
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-03 12:06:59 -0400

    Array Iteration content added
    Creating for loop and while loop examples for iterating arrays. Recommending for loops and showing the difference.

---
+++ 6 - Arrays/02 - Array Iteration/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] numbers = { 1, 1, 1, 2, 2 };
+
+/* 
+ * Recommended for iterating arrays
+ * For loops are used to repeat actions a set number of times which fits well with arrays having a fixed size 
+ */ 
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i]);
+}
+Console.WriteLine();
+
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    Console.Write(numbers[i]);
+}
+Console.WriteLine();
+
+// While/Do...While loops are used to repeat actions an unknown number of times which does not fit well with arrays having a fixed size
+var j = 0;
+while (j < numbers.Length)
+{
+    Console.Write(numbers[j]);
+    j++;
+}
+Console.WriteLine();
+
+j = numbers.Length - 1;
+while (j >= 0)
+{
+    Console.Write(numbers[j]);
+    j--;
+}
