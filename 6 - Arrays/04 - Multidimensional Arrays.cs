6b34342dcfc1bb845932aca1d27e889fc9ea122c
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-08 19:55:06 -0400

    Multidimensional Array content created, reorg
    Creating an example of a treasure map - gets iterated, conditions checked if X is surrounded by stars (*) up, down, left and right it is a hidden treasure otherwise it is not a hidden treasure. Demonstrates realistic scenario of bound checking and 2d array iteration.
    
    Also reorganizing multidimensional arrays and jagged arrays.

---
+++ 6 - Arrays/04 - Multidimensional Arrays.cs
@@ -0,0 +1,47 @@
+char[,] treasureMap = new char[7, 5]
+{
+    { '*', '*', ' ', 'X', 'X'},
+    { '*', '*', ' ', ' ', ' '},
+    { '*', '*', '*', '*', '*'},
+    { '*', '*', 'X', '*', '*'},
+    { '*', ' ', '*', '*', '*'},
+    { ' ', ' ', ' ', ' ', ' '},
+    { ' ', 'X', ' ', 'X', 'X'},
+};
+
+PrintTreasureLocations(treasureMap);
+
+/* 
+ * O(n * m) time, O(1) space
+ * n is the number of rows and m is the number of columns
+ */
+void PrintTreasureLocations(char[,] map)
+{
+    var rows = map.GetLength(0);
+    var cols = map.GetLength(1);
+
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            if (map[row, col] == 'X')
+            {
+                var isUpHidden = row > 0 && map[row - 1, col] == '*';
+                var isDownHidden = row < rows - 1 && map[row + 1, col] == '*';
+                var isLeftHidden = col > 0 && map[row, col - 1] == '*';
+                var isRightHidden = col < cols - 1 && map[row, col + 1] == '*';
+
+                var isHidden = isUpHidden && isDownHidden && isLeftHidden && isRightHidden;
+
+                if (isHidden)
+                {
+                    Console.WriteLine($"Hidden treasure found at row {row} and col {col}!");
+                }
+                else
+                {
+                    Console.WriteLine($"Treasure found at row {row} and col {col}, but its not hidden!");
+                }
+            }
+        }
+    }
+}
