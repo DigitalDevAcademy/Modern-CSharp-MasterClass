881da214da1bf3d86ba7eef83413c9ab7e2787b5
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-09 21:09:22 -0400

    Array Operations content created
    Adding most commonly used array operations along with time and space complexities for ones which are fundamental, and ones which are .NET runtime dependent mentioning as such.

---
+++ 6 - Arrays/06 - Array Operations.cs
@@ -0,0 +1,28 @@
+int[] numbers = [10, 10, 2, 2, 30, 30, 4, 4, 50, 50];
+
+// O(n) time, O(1) space
+var indexNotFound = Array.IndexOf(numbers, 100);
+Console.WriteLine($"Found Index: {indexNotFound}");
+
+var indexFound = Array.IndexOf(numbers, 50);
+Console.WriteLine($"Found Index: {indexFound}");
+
+// O(n) time, O(1) space
+var lastIndexFound = Array.LastIndexOf(numbers, 50);
+Console.WriteLine($"Found Index: {lastIndexFound}");
+
+// Time and space both .NET runtime dependent
+Array.Sort(numbers);
+var sortedAsc = string.Join(", ", numbers);
+Console.WriteLine(sortedAsc);
+
+// O(n) time, O(1) space
+Array.Reverse(numbers);
+var reveresed = string.Join(", ", numbers);
+Console.WriteLine(reveresed);
+
+// O(n) time, O(1) space
+Array.Fill(numbers, 0);
+var refilled = string.Join(", ", numbers);
+Console.WriteLine(refilled);
+
