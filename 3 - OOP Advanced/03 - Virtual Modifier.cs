dfaab5db354f42dd48ecb9ece401157e6de3af53
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-06-27 18:59:06 -0400

    Reorganizing lectures, better & general naming
    Virtual Modifier and Abstract Modifier are much more general and cleaner names.

---
+++ 3 - OOP Advanced/03 - Virtual Modifier.cs
@@ -0,0 +1,49 @@
+SchoolMember schoolMember1 = new Student("John", "Doe", 3.5m);
+SchoolMember schoolMember2 = new Teacher("Sarah", "Doe", "Computer Science");
+
+DisplaySchoolMemberInfo(schoolMember1);
+DisplaySchoolMemberInfo(schoolMember2);
+
+void DisplaySchoolMemberInfo(SchoolMember schoolMember)
+{
+    Console.WriteLine(schoolMember.GetInfo());
+}
+
+class SchoolMember
+{
+    public string FirstName { get; init; }
+
+    public string LastName { get; init; }
+
+    public SchoolMember(string make, string model)
+    {
+        FirstName = make;
+        LastName = model;
+    }
+
+    public virtual string GetInfo() => $"{FirstName} {LastName}";
+}
+
+class Student : SchoolMember
+{
+    public decimal GPA { get; init; }
+
+    public Student(string firstName, string lastName, decimal GPA) : base(firstName, lastName)
+    {
+        this.GPA = GPA;
+    }
+
+    public override string GetInfo() => $"{base.GetInfo()} {GPA}";
+}
+
+class Teacher : SchoolMember
+{
+    public string Department { get; init; }
+
+    public Teacher(string firstName, string lastName, string department) : base(firstName, lastName)
+    {
+        Department = department;
+    }
+
+    public override string GetInfo() => $"{base.GetInfo()} {Department}";
+}
