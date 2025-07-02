f60cdd2d5b8c9b6de8079215bd69d89dd3e4b270
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-01 22:31:28 -0400

    Significant refactor to make things more realistic
    This way more concepts are included into one lecture which have already been shown in OOP Fundamentals. Great for learning and keeping it as close to real-world as possible.

--- 3 - OOP Advanced/05 - Method Overloading.cs
+++ 3 - OOP Advanced/05 - Method Overloading.cs
@@ -1,46 +1,57 @@
-Student student = new Student("John", "Doe");
+CourseRepository courseRepository = new(); 
+RegistrationSystem registrationSystem = new(courseRepository);
 
-var registration1 = student.Register(1);
-var registration2 = student.Register("ABC123");
-var registration3 = student.Register(1, "I don't have all prerequisites, but comfortable with the curriculum");
-var registration4 = student.Register("CompSci101", 10);
+Student newStudent = new("John", "Doe"); 
+
+var registration1 = registrationSystem.Register(newStudent, 1);
+var registration2 = registrationSystem.Register(newStudent, "ABC123");
+var registration3 = registrationSystem.Register(newStudent, 1, "I don't have all prerequisites, but comfortable with the curriculum");
+var registration4 = registrationSystem.Register(newStudent, "CompSci101", 10);
 
 Console.WriteLine(registration1);
 Console.WriteLine(registration2);
 Console.WriteLine(registration3);
 Console.WriteLine(registration4);
 
+class RegistrationSystem(CourseRepository courseRepository)
+{
+    private readonly CourseRepository _courseRepository = courseRepository;
+
+    public string Register(Student student, string enrollmentToken)
+    {
+        var courseId = _courseRepository.FindCourseId(enrollmentToken);
+        if (courseId == -1)
+        {
+            // In real-world you would throw an exception, return an error result and/or log
+        }
+        return $"{Register(student, courseId)} | Enrollment Token ({enrollmentToken})";
+    }
+
+    public string Register(Student student, int courseId, string note) => $"{Register(student, courseId)} | Note ({note})";
+
+    public string Register(Student student, string courseName, int instructorId)
+    {
+        var courseId = _courseRepository.FindCourseId(courseName, instructorId);
+        if (courseId == -1)
+        {
+            // In real-world you would throw an exception, return an error result and/or log
+        }
+        return $"{Register(student, courseId)} | Course Name ({courseName}) | InstructorId ({instructorId})";
+    }
+
+    public string Register(Student student, int courseId) => $"Registering {student.FirstName} {student.LastName} for CourseId ({courseId})";
+}
+
 class Student(string firstName, string lastName)
 {
-    public string FirstName { get; set; } = firstName;
+    public string FirstName { get; init; } = firstName;
 
-    public string LastName { get; set; } = lastName;
+    public string LastName { get; init; } = lastName;
+}
 
-    public string Register(string enrollmentToken)
-    {
-        var courseId = FindCourseId(enrollmentToken);
-        if (courseId == -1)
-        {
-            // In real-world you would throw an exception, return an error result and/or log
-        }
-        return $"{Register(courseId)} | Enrollment Token ({enrollmentToken})";
-    }
+class CourseRepository
+{
+    public int FindCourseId(string enrollmentToken) => enrollmentToken.Equals("ABC123") ? 1 : -1;
 
-    public string Register(int courseId, string note) => $"{Register(courseId)} | Note ({note})";
-
-    public string Register(string courseName, int instructorId)
-    {
-        var courseId = FindCourseId(courseName, instructorId);
-        if (courseId == -1)
-        {
-            // In real-world you would throw an exception, return an error result and/or log
-        }
-        return $"{Register(courseId)} | Course Name ({courseName}) | InstructorId ({instructorId})";
-    }
-
-    public string Register(int courseId) => $"Registering {FirstName} {LastName} for CourseId ({courseId})";
-
-    private int FindCourseId(string enrollmentToken) => enrollmentToken.Equals("ABC123") ? 1 : -1;
-
-    private int FindCourseId(string courseName, int instructorId) => courseName.Equals("CompSci101") && instructorId == 10 ? 1 : -1;
+    public int FindCourseId(string courseName, int instructorId) => courseName.Equals("CompSci101") && instructorId == 10 ? 1 : -1;
 }
