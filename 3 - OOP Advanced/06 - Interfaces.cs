eb6a43e579f6b1b37889856dcc9f776f7c0719b8
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-02 21:57:49 -0400

    Combining Interfaces content into one
    Combining interfaces and abstract classes difference into current lecture, easier to explain things this way.

---
+++ 3 - OOP Advanced/06 - Interfaces.cs
@@ -0,0 +1,60 @@
+IPayable payableWorker1 = new Teacher("Sarah", "Doe", "Computer Science");
+IPayable payableWorker2 = new Photographer("Some Example Electrics LLC");
+IPayable payableWorker3 = new Electrician("Some Example Electrician LLC");
+
+ProcessPayments(payableWorker1);
+ProcessPayments(payableWorker2);
+ProcessPayments(payableWorker3);
+
+void ProcessPayments(IPayable payableSchoolMember) => payableSchoolMember.ReceivePayment();
+
+interface IPayable
+{
+    void ReceivePayment();
+}
+
+abstract class SchoolMember(string firstName, string lastName)
+    {
+        public string FirstName { get; init; } = firstName;
+
+        public string LastName { get; init; } = lastName;
+
+        public abstract void Participate();
+    }
+
+class Student(string firstName, string lastName, decimal gpa) : SchoolMember(firstName, lastName)
+{
+    public decimal GPA { get; init; } = gpa;
+
+    public override void Participate() => Console.WriteLine("Completing homework and taking notes.");
+}
+
+class Teacher(string firstName, string lastName, string department) : SchoolMember(firstName, lastName), IPayable
+{
+    public string Department { get; init; } = department;
+
+    public void ReceivePayment() => Console.WriteLine("Teacher has received payment.");
+
+    public override void Participate() => Console.WriteLine("Assigning homework and providing notes.");
+}
+
+abstract class ExternalServiceProvider(string company)
+{
+    public abstract void ProvideService();
+
+    public string Company { get; init; } = company;
+}
+
+class Photographer(string company) : ExternalServiceProvider(company), IPayable
+{
+    public override void ProvideService() => Console.WriteLine("Taking photoes.");
+
+    public void ReceivePayment() => Console.WriteLine("Photographer has received payment.");
+}
+
+class Electrician(string company) : ExternalServiceProvider(company), IPayable
+{
+    public override void ProvideService() => Console.WriteLine("Fixing electricity.");
+
+    public void ReceivePayment() => Console.WriteLine("Electrician has received payment.");
+}
