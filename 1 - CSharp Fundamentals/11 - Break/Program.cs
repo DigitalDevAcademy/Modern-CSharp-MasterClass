1a937b2b12d65d4419cb15884c820034bca4e06a
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-25 12:02:18 -0400

    Removed comment
    Initially comment seemed helpful, but upon current review it is not.

--- 1 - CSharp Fundamentals/11 - Break/Program.cs
+++ 1 - CSharp Fundamentals/11 - Break/Program.cs
@@ -1,8 +1,6 @@
-﻿// Only showcasing break in for loop, however it can be used in other types of loops
-
-for (int page = 1; page <= 10; page++)
+﻿for (int page = 1; page <= 10; page++)
 {
-    if (page == 5) // Stop the loop when page is 5
+    if (page == 5)
     {
         break;
     }
