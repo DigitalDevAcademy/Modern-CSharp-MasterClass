ab8daef26d676f22192cfbffa3b3233ab837b5ec
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-25 11:10:04 -0400

    Removed comment
    Initially comment seemed helpful, but upon current review it is not.

--- 1 - CSharp Fundamentals/10 - Do While Loop/Program.cs
+++ 1 - CSharp Fundamentals/10 - Do While Loop/Program.cs
@@ -1,11 +1,5 @@
 ﻿var input = string.Empty;
 
-/*
- We only ask for user input once at the beginning
- After that, the do-while loop will run an unknown number of times — 
- everything depends on the user's input
-*/
-
 do
 {
     input = Console.ReadLine();
