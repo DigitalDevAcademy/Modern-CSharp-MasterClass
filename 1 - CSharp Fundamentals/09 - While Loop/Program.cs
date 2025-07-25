e9f1a4b7b4a1a090ffd31b97684844095d5b429d
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-24 22:46:16 -0400

    Removed comment
    Initially comment seemed helpful, but upon current review it is not.

--- 1 - CSharp Fundamentals/09 - While Loop/Program.cs
+++ 1 - CSharp Fundamentals/09 - While Loop/Program.cs
@@ -1,7 +1,6 @@
 ﻿var input = Console.ReadLine();
 Console.WriteLine(input);
 
-// The while loop will run an unknown number of times — everything depends on the user's input
 while (!string.IsNullOrEmpty(input))
 {
     input = Console.ReadLine();
