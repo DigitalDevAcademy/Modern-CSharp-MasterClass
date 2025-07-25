b090024d76bcb8b02cc0ba5bc405b31a1d8791f7
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-25 13:18:01 -0400

    Removed comment
    Initially comment seemed helpful, but upon current review it is not.

--- 1 - CSharp Fundamentals/12 - Continue/Program.cs
+++ 1 - CSharp Fundamentals/12 - Continue/Program.cs
@@ -1,8 +1,6 @@
-﻿// Only showcasing continue in for loop, however it can be used in other types of loops
-
-for (int page = 1; page <= 10; page++)
+﻿for (int page = 1; page <= 10; page++)
 {
-    if (page == 5) // Skip page 5, continue the loop
+    if (page == 5)
     {
         continue;
     }
