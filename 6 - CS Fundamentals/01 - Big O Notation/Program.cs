708f368bc569cfc70d7a93ba2a89e628963342dc
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-25 20:12:33 -0400

    Clarified and shortened comments
    Some of the comments were helpful, but some were removed and shortened for better clarity.

--- 6 - CS Fundamentals/01 - Big O Notation/Program.cs
+++ 6 - CS Fundamentals/01 - Big O Notation/Program.cs
@@ -4,10 +4,10 @@ Work2(10);
 Work3(5, 10);
 Work4(5, 10);
 
-// O(1) time - both declaration and printing of the value are constant operations
+// O(1) time
 void Work1(int input) => Console.WriteLine(input);
 
-// O(n) time – number of operations grows at the same rate as 'itemLimit'
+// O(n) time
 void Work2(int input)
 {
     for (int i = 0; i < input; i++)
@@ -16,7 +16,7 @@ void Work2(int input)
     }
 }
 
-// O(n + m) time – number of operations grow at the same rate as 'input1' and 'input2' - sequentially
+// O(n + m) time
 void Work3(int input1, int input2)
 {
     for (int i = 0; i < input1; i++)
@@ -30,7 +30,7 @@ void Work3(int input1, int input2)
     }
 }
 
-// For each row, we go through all columns, making this O(n * m) time – or O(r * c) time to keep it clear
+// O(n * m) or O(r * c) time to keep it clear
 void Work4(int input1, int input2)
 {
     for (int row = 0; row < input1; row++)
