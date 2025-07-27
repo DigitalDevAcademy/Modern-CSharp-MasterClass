e1a137a0c576a9860bc5820e578a73c83e91da2d
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-27 19:33:26 -0400

    Jagged Array comment clean up
    Comment needed cleanup, only show time and space complexity.

--- 7 - Arrays/05 - Jagged Array/Program.cs
+++ 7 - Arrays/05 - Jagged Array/Program.cs
@@ -7,11 +7,7 @@
 
 DisplayClassGrades(allClassGrades);
 
-/* 
- * O(n * m) time, O(1) space
- * n is the number of arrays within the jagged array and m is the length of each array
- * As mentioned we estimate the worse case where all array lengths are equal
- */
+// O(n * m) time, O(1) space
 void DisplayClassGrades(int[][] classGrades)
 {
     for (int classIdx = 0; classIdx < classGrades.Length; classIdx++)
