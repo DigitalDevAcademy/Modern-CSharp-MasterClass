dea9a477db57c7616c46c76558894628eddb1632
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-29 18:49:05 -0400

    Cleaned comments
    Some of the comments were not helpful upon review, others were which I left in place.

--- 8 - Strings/05 - StringBuilder/Program.cs
+++ 8 - Strings/05 - StringBuilder/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System.Text;
 
-// This simple list could be something we get from a database or another source
 string[] reportParts = ["many", "parts", "of", "a", "report"];
 
-// Initializing a new StringBuilder
 StringBuilder sb = new();
 
 // This is how you set the capacity
@@ -12,7 +10,6 @@ StringBuilder sb = new();
 // This is how you set an initial string and set the capacity
 // StringBuilder sb = new("Some initial value", 500);
 
-// Iterating through parts of the report to build the full version
 for (int i = 0; i < reportParts.Length; i++)
 {
     sb.AppendLine(reportParts[i]);
