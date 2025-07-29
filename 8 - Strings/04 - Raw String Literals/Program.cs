91881dcb5bfdbd53049ee0e250fb770cf84e5459
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-29 14:01:01 -0400

    Raw String Literal comments cleaned and example added
    I added a separate example of how to escape """ or more if needed. Comments were not helpful upon review so removed them.

--- 8 - Strings/04 - Raw String Literals/Program.cs
+++ 8 - Strings/04 - Raw String Literals/Program.cs
@@ -1,11 +1,7 @@
 ﻿var fileDetails = string.Empty;
 var fileSize = "4 KB";
 
-/* Auto escaping backslashes, double quotes and more 
- * Also creating new lines with raw string literals
- * Introduced during C# 11 .NET 7
- */
-fileDetails = $""" 
+fileDetails = """ 
     File Location: "C:\Documents\File.txt"
     File Size: 4 KB
     """;
@@ -13,11 +9,17 @@ Console.WriteLine(fileDetails);
 
 Console.WriteLine();
 
-// Interpolated raw strings literals have the above mentioned benefits + interpolation
+fileDetails = """" 
+    """File Location: "C:\Documents\File.txt"""
+    """File Size: 4 KB"""
+    """";
+Console.WriteLine(fileDetails);
+
+Console.WriteLine();
+
 fileDetails = $$""""
     File Location: "C:\Documents\File.txt"
     File Size: {{{fileSize}}}
-    """ More file information """
     { Not interpolated }
     """";
 Console.WriteLine(fileDetails);
