6ada769230ac4fe257e0f81abbfc0e3ac02617c6
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-28 21:46:19 -0400

    Escape Sequences comments removed and improvement
    Some of the comments needed cleanup and added Unicode escape sequence example.

--- 8 - Strings/02 - Escape Sequences/Program.cs
+++ 8 - Strings/02 - Escape Sequences/Program.cs
@@ -1,10 +1,7 @@
-﻿// Most common escape sequences
+﻿Console.WriteLine("File location is C:\\Documents\\File.txt");
 
-// Escaping the backslash using backslash escape character
-Console.WriteLine("File location is C:\\Documents\\File.txt");
-
-// Escaping the double quotes using backslash escape character
 Console.WriteLine("Here is a quote \"Hello World\"");
 
-// Writing text on two separate lines by using \n escape sequence
 Console.WriteLine("This is the first line\nThis is the second line");
+
+Console.WriteLine("\u2713 string and char are not the same type \u2717 string and char are the same type");
