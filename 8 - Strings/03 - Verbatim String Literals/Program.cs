d01f3b13f31cbd94cfdf6c6e0ea5f2b40ef7b377
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-07-28 22:33:04 -0400

    Removed comment
    Initially comment seemed helpful, but upon current review it is not.

--- 8 - Strings/03 - Verbatim String Literals/Program.cs
+++ 8 - Strings/03 - Verbatim String Literals/Program.cs
@@ -1,17 +1,10 @@
 ﻿var fileDetails = string.Empty;
 var fileSize = "4 KB";
 
-// Auto escaping backslashes and creating new lines with verbatim string literals
 fileDetails = @"File Location: ""C:\Documents\File.txt""
                 File Size: 4 KB";
 Console.WriteLine(fileDetails);
 
-Console.WriteLine();
-
-/* Interpolated verbatim string literals have the above mentioned benefits + interpolation
- * These were introduced during C# 6 but after C# 8 .NET Core 3 
- * ($@ or @$) order does not matter
- */
 fileDetails = $@"File Location: ""C:\Documents\File.txt"" 
                  File Size: {fileSize}
                  {{ No interpolated }}";
