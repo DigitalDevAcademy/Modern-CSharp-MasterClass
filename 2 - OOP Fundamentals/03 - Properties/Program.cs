dcdceeb0950983e599f3a8293bb9bf68dcb5fff5
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-08-02 20:15:30 -0400

    Clarified and cleaned comments for Properties
    Some of the comments I removed as they are already explained in the lecture, other ones I clarified upon reviewing.

--- 2 - OOP Fundamentals/03 - Properties/Program.cs
+++ 2 - OOP Fundamentals/03 - Properties/Program.cs
@@ -11,25 +11,20 @@ bus.Operator = "Another Operator";
 Console.WriteLine(bus.Operator);
 // bus.Capacity = 100; // Cannot modify Capacity property because it is immutable
 Console.WriteLine(bus.Capacity);
-// bus.Manufacturer = "Another Bus Company"; // Cannot modify Manufacturer because it is a read-only property
+// bus.Manufacturer = "Another Bus Company"; // Cannot modify Manufacturer because it is a read-only auto-property
 Console.WriteLine(bus.Manufacturer);
 
 class Bus
 {
-    // Setting a default for the backing field
     private string _name = "Default Bus"; 
 
-    /* Property using the backing field.
-     * We could have also separated get or set accessors into separate properties making them read-only/write-only respectively
-     * We could have also used init instead of set making this property immutable if needed
-     */
     public string Name
     {
         get { return _name; }
         set { _name = string.IsNullOrWhiteSpace(value) ? "Default Bus" : value; }
     }
 
-    // Auto-property with get and set accessors    
+    // Auto-property with get and set accessors 
     public string Operator { get; set; } = string.Empty;
 
     // Auto-property with get and init accessors
