9fb3c7a22e789184917ed5fe5c18b301a5705f14
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-08-05 21:11:37 -0400

    The version is already mentioned in lecture, removing comment
    In order to clean things further removing the version in code example and only leaving it in lecture.

--- 4 - Structs & Enums/04 - Explicit Parameterless Constructor/Program.cs
+++ 4 - Structs & Enums/04 - Explicit Parameterless Constructor/Program.cs
@@ -6,7 +6,6 @@ readonly struct Point
     public int X { get; init; }
     public int Y { get; init; }
 
-    // C# 10 .NET 6 introduced explicit parameterless constructor for structs
     public Point()
     {
         X = 10;
