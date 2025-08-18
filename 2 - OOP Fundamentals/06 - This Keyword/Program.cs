a1cd7541f77332f98b22af4d0f20269202f7c392
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-08-18 16:42:35 -0400

    Smartphone example added for This Keyword
    Recreated example to make it more intuitive and aligned with the real-world, this will make things clearer for the students.

--- 2 - OOP Fundamentals/06 - This Keyword/Program.cs
+++ 2 - OOP Fundamentals/06 - This Keyword/Program.cs
@@ -1,31 +1,39 @@
-﻿Manager manager;
+﻿Smartphone phone1 = new();
+Console.WriteLine($"Phone 1: {phone1.Model}, {phone1.Color}, {phone1.StorageCapacity}GB");
 
-manager = new();
-Console.WriteLine($"Manager Info: {manager.FullName}, {manager.Age}");
+Smartphone phone2 = new("Pro");
+Console.WriteLine($"Phone 2: {phone2.Model}, {phone2.Color}, {phone2.StorageCapacity}GB");
 
-manager = new("John Doe");
-Console.WriteLine($"Manager Info: {manager.FullName}, {manager.Age}");
+Smartphone phone3 = new("Pro", "Silver");
+Console.WriteLine($"Phone 3: {phone3.Model}, {phone3.Color}, {phone3.StorageCapacity}GB");
 
-manager = new("John Doe", 40);
-Console.WriteLine($"Manager Info: {manager.FullName}, {manager.Age}");
+Smartphone phone4 = new("Pro", "Silver", 512);
+Console.WriteLine($"Phone 4: {phone4.Model}, {phone4.Color}, {phone4.StorageCapacity}GB");
 
-class Manager
+class Smartphone
 {
-    public string FullName { get; set; }
+    public string Model { get; init; }
 
-    public int Age { get; set; }
+    public string Color { get; init; }
 
-    public Manager() : this("Default Name")
+    public int StorageCapacity { get; init; }
+
+    public Smartphone() : this("Standard")
     {
     }
 
-    public Manager(string fullName) : this(fullName, 30)
+    public Smartphone(string model) : this(model, "White")
     {
     }
 
-    public Manager(string fullName, int age)
+    public Smartphone(string model, string color) : this(model, color, 64)
     {
-        FullName = fullName;
-        Age = age;
+    }
+
+    public Smartphone(string model, string color, int storageCapacity)
+    {
+        Model = model;
+        Color = color;
+        StorageCapacity = storageCapacity;
     }
 }
