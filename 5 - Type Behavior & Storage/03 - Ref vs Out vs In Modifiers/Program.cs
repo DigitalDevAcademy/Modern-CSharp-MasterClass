62fd89c5852e6830271a0d3e0740ae4b6e03f88c
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-08-30 20:17:20 -0400

    Cleaning up comments in Ref vs Out vs In Modifiers
    The versions are already mentioned in the intro section of the lecture, so removing them from comments.

--- 5 - Type Behavior & Storage/03 - Ref vs Out vs In Modifiers/Program.cs
+++ 5 - Type Behavior & Storage/03 - Ref vs Out vs In Modifiers/Program.cs
@@ -3,7 +3,6 @@ ChangeUsingRef(ref firstNumber);
 void ChangeUsingRef(ref int number) => number = 2;
 Console.WriteLine(firstNumber);
 
-// Starting with C# 7.0 you can declare out variables inline
 var result = ChangeUsingOut(out int secondNumber);
 bool ChangeUsingOut(out int number)
 {
@@ -13,6 +12,5 @@ bool ChangeUsingOut(out int number)
 Console.WriteLine($"{result} - {secondNumber}");
 
 int thirdNumber = 1;
-// Starting with C# 7.2 you can declare method params as read-only, recommended for value-types only
 AccessUsingIn(in thirdNumber);
 void AccessUsingIn(in int number) => Console.WriteLine(number); // number = 2; // <-- Results in compile-time error
