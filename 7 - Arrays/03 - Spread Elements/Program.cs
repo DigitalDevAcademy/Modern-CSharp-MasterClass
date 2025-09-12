9940318aa50f1d80dc9060b0d4c0ac230beb1e20
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-09-12 16:09:16 -0400

    Renaming for better clarity + small edits
    combinedData makes much more sense for the variable, also changing some of the numbers which were entered differently during video recording.

--- 7 - Arrays/03 - Spread Elements/Program.cs
+++ 7 - Arrays/03 - Spread Elements/Program.cs
@@ -1,11 +1,11 @@
 ﻿decimal[] q1Sales = [123.45m, 102.30m];
-decimal[] q2Sales = [89.99m, 76.50m, 98.75m, 110.00m];
+decimal[] q2Sales = [89.99m, 76.50m, 98.75m, 120.00m];
 decimal[] q3Sales = [105.60m, 99.99m, 92.45m];
-decimal[] q4Sales = [67.89m, 80.00m, 115.25m, 108.40m, 120.00m];
-decimal[] allSales = CombineSalesAndBonuses(q1Sales, q2Sales, q3Sales, q4Sales);
+decimal[] q4Sales = [67.89m, 80.00m, 115.25m, 108.40m, 130.00m];
+decimal[] combinedData = CombineSalesAndBonuses(q1Sales, q2Sales, q3Sales, q4Sales);
 
-Console.WriteLine(string.Join(",", allSales));
+Console.WriteLine(string.Join(",", combinedData));
 
 // Time and space O(n + m + k + l)
 decimal[] CombineSalesAndBonuses(decimal[] q1Sales, decimal[] q2Sales, decimal[] q3Sales, decimal[] q4Sales)
-    => [.. q1Sales, 1000m, .. q2Sales, 2500m, .. q3Sales, 4000m, .. q4Sales, 5500m];
+    => [..q1Sales, 1000m, ..q2Sales, 2500m, ..q3Sales, 4000m, ..q4Sales, 5500m];
