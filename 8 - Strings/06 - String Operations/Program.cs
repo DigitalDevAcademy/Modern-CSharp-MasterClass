d10c19393c773c6856d90134cfc8b8dc08b49c3d
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-09-18 11:44:38 -0400

    Providing string methods with time/space complexities
    All of these methods have linear growth in general. Some of them are .NET runtime dependent as mentioned. Others have multiple variables which I simplify to O(n). Rest have time and space complexities denotes as usual.

--- 8 - Strings/06 - String Operations/Program.cs
+++ 8 - Strings/06 - String Operations/Program.cs
@@ -2,31 +2,57 @@
 string substring = "modern";
 string untrimmedText = "  Example, Text!  ";
 
+// Time .NET runtime dependent, O(1) space
 Console.WriteLine(text.IndexOf(substring));
+
+// Time .NET runtime dependent, O(1) space
 Console.WriteLine(text.Contains("learned"));
+
+// O(v) time, O(1) space
 Console.WriteLine(text.StartsWith("I"));
+
+// O(v) time, O(1) space
 Console.WriteLine(text.EndsWith("C#"));
+
+// Time .NET runtime dependent, O(m) space
 Console.WriteLine(text.Replace("learned", "mastered"));
+
+// O(n) time, O(n) space
 Console.WriteLine(text.ToLower());
+
+// O(n) time, O(n) space
 Console.WriteLine(text.ToUpper());
+
+// O(1) time, O(1) space
 Console.WriteLine(string.IsNullOrEmpty(string.Empty));
+
+// O(n) time, O(1) space
 Console.WriteLine(string.IsNullOrWhiteSpace("   "));
+
+// O(n) time, O(m) space
 Console.WriteLine("[" + untrimmedText.Trim() + "]");
+
+// O(n) time, O(m) space
 Console.WriteLine("[" + untrimmedText.TrimStart() + "]");
+
+// O(n) time, O(m) space
 Console.WriteLine("[" + untrimmedText.TrimEnd() + "]");
 
+// O(n) time, O(n) space
 string[] parts = text.Split(' ');
 foreach (string part in parts) 
 {
     Console.WriteLine(part);
 }
 
+// O(m) time, O(m) space
 string joinedTextByWhiteSpaces = string.Join(" ", parts);
 string joinedTextByDashes = string.Join("-", parts);
 
 Console.WriteLine(joinedTextByWhiteSpaces);
 Console.WriteLine(joinedTextByDashes);
 
+// O(n) time, O(n) space
 char[] textToCharArray = text.ToCharArray();
 foreach (char character in textToCharArray)
 {
