3be62428913c085bac61a1bac39e1089b2b5c842
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-10-13 17:58:47 -0400

    Complete example for Field-Backed Properties from C# 14 .NET 10
    Coding example demonstrates how field-backed properties can be used to replace some functionality. But also demonstrates its limitations.

--- 2 - OOP Fundamentals/04 - Field-Backed Properties/Program.cs
+++ 2 - OOP Fundamentals/04 - Field-Backed Properties/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Employee employee = new("John", "Doe", "Marketing");
+Console.WriteLine(employee.Badge);
+Console.WriteLine(employee.Departement);
+employee.Departement = "Sales";
+Console.WriteLine(employee.Departement);
+
+class Employee
+{
+    private string _firstName;
+
+    private string _lastName;
+
+    private string _departement;
+
+    public Employee(string firstName, string lastName, string departement)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        _departement = departement;
+    }
+
+    public string Badge
+    {
+        get { return $"{_firstName} {_lastName} {_departement}"; }
+    }
+
+    public string Departement
+    {
+        get { return _departement; }
+        set { _departement = string.IsNullOrWhiteSpace(value) ? _departement : value; }
+    }
+
+    // public string Departement
+    // {
+    //     get { return field; }
+    //     set { field = string.IsNullOrWhiteSpace(value) ? field : value; }
+    // }
+}
