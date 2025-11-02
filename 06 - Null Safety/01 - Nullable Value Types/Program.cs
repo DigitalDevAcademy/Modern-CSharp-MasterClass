85b3eda92673741cfc4cc6ca07ab0d049f884a22
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-11-02 12:16:07 -0500

    Adding example for nullable value types
    Keeping the concept simple to showcase the benefits of nullability for value types.

--- 06 - Null Safety/01 - Nullable Value Types/Program.cs
+++ 06 - Null Safety/01 - Nullable Value Types/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿User user1 = new() { FirstName = "John", LastName = "Doe" };
+User user2 = new() { FirstName = "Mark", LastName = "Doe",  Age = 30};
+
+PrintAgeInfo(user1);
+PrintAgeInfo(user2);
+
+void PrintAgeInfo(User user)
+{
+    if (user.Age is int age)
+    {
+        Console.WriteLine($"{user.FirstName} {user.LastName} is {age} years old.");
+    }
+    else
+    {
+        Console.WriteLine($"{user.FirstName} {user.LastName} did not specify their age.");
+    }
+}
+
+class User
+{
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public int? Age { get; set; }
+}
