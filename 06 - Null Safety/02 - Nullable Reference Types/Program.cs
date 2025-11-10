fe37f72f75edf3dc01540c0da8a62dd11b6d4789
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-11-09 21:54:21 -0500

    Adding Nullable Reference Types example
    Complete example demonstrating the value of nullable reference types.

--- 06 - Null Safety/02 - Nullable Reference Types/Program.cs
+++ 06 - Null Safety/02 - Nullable Reference Types/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Address someAddress = new("123 Main St", "SomeCity", "SomeState", "12345", "US");
+
+Person person1 = new("John", "Doe", someAddress);
+Person person2 = new("Mark", "Doe");
+
+PersonCityFinder(person1);
+PersonCityFinder(person2);
+
+void PersonCityFinder(Person person)
+{
+    if (person.CurrentAddress is Address a)
+    {
+        Console.WriteLine($"{person.FirstName} {person.LastName} is currently in {a.City} city.");
+    }
+    else
+    {
+        Console.WriteLine($"{person.FirstName} {person.LastName} does not have a current address, so cannot show the city.");
+    }
+}
+
+class Person
+{
+    public Person(string firstName, string lastName) : this(firstName, lastName, null)
+    {
+    }
+
+    public Person(string firstName, string lastName, Address? address)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        CurrentAddress = address;
+    }
+
+    public string FirstName { get; init; }
+
+    public string LastName { get; init; }
+    
+    public Address? CurrentAddress { get; set; }
+}
+
+class Address(string addressLine, string city, string state, string postalCode, string country)
+{
+    public string AddressLine { get; init; } = addressLine;
+    public string City { get; init; } = city;
+    public string State { get; init; } = state;
+    public string PostalCode { get; init; } = postalCode;
+    public string Country { get; init; } = country;
+}
