300b459600185157e63b6f0f5a2ec0a4c25c653d
Digital Dev Academy <176520070+DigitalDevAcademy@users.noreply.github.com>
2025-11-14 18:38:55 -0500

    Null-Conditional Operator code example created
    Demonstrating a general config example, which is similar to many real-world scenarios. Also, showcasing the benefit of using the operator.

--- 06 - Null Safety/05 - Null-Conditional Operator/Program.cs
+++ 06 - Null Safety/05 - Null-Conditional Operator/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿SomeConfig someConfig = new();
+someConfig.Root = new(1);
+someConfig.Root.Child = new(2);
+someConfig.Root.Child.Child = new(3);
+someConfig.Root.Child.Child.Child = new(4);
+
+// if (someConfig is SomeConfig sc)
+// {
+//     if (sc.Root is Node root)
+//     {
+//         if (root.Child is Node node1)
+//         {
+//             if (node1.Child is Node node2)
+//             {
+//                 if (node2.Child is Node node3)
+//                 {
+//                     Console.WriteLine(node3.Value);
+//                 }   
+//             }
+//         }
+//     }
+// }
+
+Console.WriteLine(someConfig.Root?.Child?.Child?.Child?.Value);
+
+class SomeConfig
+{
+    public Node? Root { get; set; }
+}
+
+class Node
+{
+    public Node(int value)
+    {
+        Value = value;
+    }
+
+    public Node? Child { get; set; }
+
+    public int Value { get; set; }
+}
